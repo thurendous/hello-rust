37f2c18cd949adb66b5829b1b8604178262503b9
thurendous <thurendous2@gmail.com>
2025-01-13 22:17:26 +0900

    add code

---
+++ src/bin/enum.rs
@@ -0,0 +1,17 @@
+#![allow(unused_variables)]
+
+fn main() {
+  // Enum
+  enum Color {
+    Red,
+    Green,
+    Blue,
+    Rgba(u8, u8, u8, u8),
+  }
+
+  // Attributes
+
+  // Option
+
+  // Result
+}
